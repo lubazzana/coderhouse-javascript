1ba540aff373888063f036fb6ce80ec1c4d94678
lubazzana <lucreciabazzana@gmail.com>
2021-11-04 01:50:13 -0300

    entrega 4 desafio corregida

--- entrega-clase4-Bazzana/simulador.js
+++ entrega-clase4-Bazzana/simulador.js
@@ -5,12 +5,15 @@ function compra () {
     let cuotas = Number(prompt(`¿En cuántas cuotas los querés abonar?
 Tené en cuenta que si lo abonás en más de 3 cuotas, se agregará un recargo del 5% al valor total`));
 
-    let precioCuota= precio/cuotas;
-    
-    if (cuotas > 3) {
-        let recargo = Math.round((precioCuota + 1*5/100)*100) /100;
+    let valorCuota = precio/cuotas;
+
+    if (cuotas <= 3) {
+        let totalCuota = Math.round(valorCuota*100) /100;
+        return alert(`Si lo abonás en ${cuotas} cuotas, el valor de cada cuota será de $${totalCuota}`)
+    } else {
+        let recargo = Math.round((valorCuota + 1*5/100)*100) /100;
         alert(`Si lo abonás en ${cuotas} cuotas, el valor de cada cuota será de $${recargo}`)
-}
+    }
 }
 
 compra()
