79c98d1600f6b957dad91781336a21dc21f43611
lubazzana <lucreciabazzana@gmail.com>
2021-12-02 21:44:15 -0300

    segunda entrega

--- proyecto-final/segunda-entrega-Bazzana/app.js
+++ proyecto-final/segunda-entrega-Bazzana/app.js
@@ -56,3 +56,68 @@ function setSuccess(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
 }
+
+
+
+class Producto {
+    constructor (id, nombre, precio, categoria, imagen){
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.categoria = categoria;
+    this.imagen = imagen;
+    }
+}
+
+let productos = [];
+productos.push(new Producto(1, 'Café', 25, 'Cafetería', './images/fondo-portada.jpg'));
+productos.push(new Producto(2, 'Café', 65, 'Cafetería', './images/fondo-portada.jpg'));
+productos.push(new Producto(3, 'Café', 40, 'Cafetería', './images/fondo-portada.jpg'));
+productos.push(new Producto(4, 'Café', 90, 'Cafetería', './images/fondo-portada.jpg'));
+productos.push(new Producto(5, 'Café', 210, 'Cafetería', './images/fondo-portada.jpg'));
+
+
+let mostrarProductos = document.querySelector("#cartaCafe");
+let nuevoDiv = ""; //document.createElement("div")
+
+for(let i = 0; i < productos.length; i++){
+    
+    nuevoDiv+= `
+	<div class="col-md-3 m-0">
+		<div class="card mt-2 mb-2" style="width: 18rem;">
+			<img src="${productos[i].imagen}" class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">${productos[i].nombre}</h5>
+				<p class="card-text">$${productos[i].precio}</p>
+				<p class="card-text">${productos[i].categoria}</p>
+				<button id="add-cart-btn" class="btn btn-primary" onclick="guardar(${productos[i].id})">Agregar al pedido</button>
+			</div>
+		</div>
+	</div>
+    `
+}
+
+mostrarProductos.innerHTML = nuevoDiv;
+
+let pedido = [];
+
+function guardar(id) {
+	console.log('guardar');
+	if (localStorage.getItem('order')) {
+		pedido = JSON.stringify(localStorage.getItem('order'));
+	}
+
+	pedido.push(productos.map(el => el.id == id));
+	localStorage.setItem('order', JSON.stringify(pedido));
+}
+
+// document.getElementById("add-cart-btn").addEventListener('click', e=> {
+// 	e.preventDefault();
+
+// 	if (localStorage.getItem('order')) {
+// 		pedido = JSON.stringify(localStorage.getItem('order'));
+// 	}
+
+// 	pedido.push()
+// 	localStorage.setItem('order', 'hola');
+// })
